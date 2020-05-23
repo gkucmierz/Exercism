4b5704d38e143ebf50d1e6b133679363603e025c
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-05-23 22:27:33 +0200

    prime-factors

---
+++ javascript/prime-factors/prime-factors.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Prime Factors' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const primeFactors = n => {
+  if (n < 2) return [];
+  const res = [];
+  let max = Math.floor(Math.sqrt(n));
+  for (let i = 2; i <= max; ++i) {
+    if (n % i === 0) {
+      res.push(i);
+      n /= i;
+      max = Math.floor(Math.sqrt(n));
+      i = (Math.min(...res) || 2) - 1;
+    }
+  }
+  res.push(n);
+  return res;
+};
