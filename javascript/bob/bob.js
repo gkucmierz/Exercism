f48bf6ddb79458a5599402e7398e3b92bdc6ba94
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-05-24 18:35:27 +0200

    bob

---
+++ javascript/bob/bob.js
@@ -0,0 +1,30 @@
+//
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const MSG_SURE = 'Sure.';
+const MSG_WHOA = 'Whoa, chill out!';
+const MSG_WHATEVER = 'Whatever.';
+const MSG_CALM = 'Calm down, I know what I\'m doing!';
+const MSG_FINE = 'Fine. Be that way!';
+
+export const hey = message => {
+  const trimmed = message.trim();
+
+  const upper = trimmed === trimmed.toUpperCase() && trimmed.match(/[A-Z]/);
+  const question = trimmed.match(/\?$/);
+  const exclam = trimmed.match(/!$/);
+  const numbers = trimmed.match(/^[0-9\s,!\?]+$/);
+  const empty = trimmed === '';
+
+  if (empty) return MSG_FINE;
+  if (numbers && question) return MSG_SURE;
+  if (numbers) return MSG_WHATEVER;
+  if (upper && question) return MSG_CALM;
+  if (upper) return MSG_WHOA;
+  if (exclam) return MSG_WHATEVER;
+  if (question) return MSG_SURE;
+  
+  return MSG_WHATEVER;
+};
