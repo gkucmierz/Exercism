962d29eead8c10d92607352304aa38f65c45a346
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-05-25 12:36:08 +0200

    go: to-fer: add comments

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,13 +1,10 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
 
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package that can produce sentence
 package twofer
 
 import "fmt"
 
-// ShareWith should have a comment documenting it.
+// Produce share sentence
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
